6c2e90672b70152c986ff565e0199a56f84f6eb2
koashima <48111257+koashima@users.noreply.github.com>
2020-11-23 22:56:06 -0800

    Sets up basic routing

--- chaire-frontend/src/App.js
+++ chaire-frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
@@ -5,12 +6,17 @@ import Chat from './components/Chat/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <h1>CHAIRE</h1>
-      <Login />
-      <Register />
-      <Chat />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>CHAIRE</h1>
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route render={() => <h1>BIG OOF AKA 404 PAGE NOT FOUND</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
