5d806956461c7eed0e25903a08794cbaf9950fe4
koashima <48111257+koashima@users.noreply.github.com>
2020-11-23 23:46:52 -0800

    preliminary login page

--- chaire-frontend/src/components/Auth/Login.js
+++ chaire-frontend/src/components/Auth/Login.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import loginImage from '../../assets/images/welcome.svg';
 
 const Login = () => {
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <div class="auth-container">
+      <div class="auth-card">
+        <div>
+          <div class="image-section">
+            <h2>HELLO OR CHAIRE!</h2>
+            <img src={loginImage} alt="login" style={{ width: 300 }} />
+          </div>
+
+          <div class="form-section">
+            <h2>LOGIN</h2>
+            <form action="">
+              <div class="input-field">
+                <input placeholder="EMAIL" />
+              </div>
+              <div class="input-field">
+                <input placeholder="PASSWORD" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
